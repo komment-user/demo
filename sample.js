d4a2b06d9018f5f8523cc8a151c0a5656d61eed8
Nathan Bateman <150971763+komment-user@users.noreply.github.com>
2023-12-28 13:26:54 +0100

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,188 @@
+
+/**
+* @description This function fetches the top posts from a subreddit specified by the
+* `sub` parameter using Axios to make an HTTP GET request to the Reddit API. It then
+* logs the response data and returns it.
+* 
+* @param { string } sub - The `sub` input parameter is a string that specifies the
+* subreddit to fetch data from.
+* 
+* @returns { object } The output of the `fetch` function is a promise that resolves
+* to an object containing the data from the Reddit API.
+*/
+function fetch(sub = 'programming') {
+    const axios = require('axios')
+
+    axios.get(`https://www.reddit.com/r/\${sub}.json`)
+    .then((response) => {
+        console.log(response);
+        return response;
+    })
+    .catch((error) => {
+        console.error(error);
+        return null;
+    });
+}
+
+
+/**
+* @description This function performs a binary search on an array `arr` for the value
+* `x`.
+* 
+* @param { array } arr - The `arr` input parameter is the array to be searched.
+* 
+* @param { any } x - The `x` input parameter is the value to be searched for within
+* the array.
+* 
+* @param { number } start - The `start` input parameter determines the left boundary
+* of the subarray to be searched.
+* 
+* @param { number } end - The `end` input parameter specifies the endpoint of the
+* search range.
+* 
+* @returns { boolean } This function takes an array `arr`, a target value `x`, and
+* three integers `start`, `end`, and `mid` as parameters. It uses a recursive binary
+* search algorithm to find the index of `x` within `arr`.
+* 
+* The output returned by this function is `true` or `false`, depending on whether
+* `x` is found within `arr` or not.
+*/
+const search = (arr, x, start, end) => {
+  if (start > end) return false;
+  let mid = Math.floor((start + end)/2);
+
+  if (arr[mid]===x) return true;
+        
+  if (arr[mid] > x) {
+    return search(arr, x, start, mid-1);
+  } else {
+    return search(arr, x, mid+1, end);
+  }
+}
+
+
+/**
+* @description This is an AWS Lambda function that handles various routes for FIDO2
+* authentication.
+* 
+* @param { object } event - The `event` input parameter is an object that contains
+* information about the current HTTP request or API call. It provides context and
+* metadata about the event that triggered the function call.
+* 
+* @returns { object } The output of this function is a JSON object with the following
+* properties:
+* 
+* 	- `statusCode`: an integer value indicating the HTTP status code to be returned
+* (200 if successful or one of 40x/50x if there is an error)
+* 	- `body`: a string containing the body of the HTTP response
+* 	- `headers`: an object containing custom HTTP headers.
+* 
+* The function returns this output based on the incoming request event and the claim
+* values obtained from the authorization token.
+*/
+const handler = async(event) => {
+    try {
+        const { sub, email, phone_number: phoneNumber, name, "cognito:username": cognitoUsername, } = event.requestContext.authorizer.jwt.claims;
+        const userHandle = determineUserHandle({ sub, cognitoUsername });
+        const userName = email ?? phoneNumber ?? name ?? cognitoUsername;
+        const displayName = name ?? email;
+        if (event.pathParameters.fido2path === "register-authenticator/start") {
+            logger.info("Starting a new authenticator registration ...");
+            if (!userName) {
+                throw new Error("Unable to determine name for user");
+            }
+            if (!displayName) {
+                throw new Error("Unable to determine display name for user");
+            }
+            const rpId = event.queryStringParameters?.rpId;
+            if (!rpId) {
+                throw new UserFacingError("Missing RP ID");
+            }
+            if (!allowedRelyingPartyIds.includes(rpId)) {
+                throw new UserFacingError("Unrecognized RP ID");
+            }
+            const options = await requestCredentialsChallenge({
+                userId: userHandle,
+                name: userName,
+                displayName,
+                rpId,
+            });
+            logger.debug("Options:", JSON.stringify(options));
+            return {
+                statusCode: 200,
+                body: JSON.stringify(options),
+                headers,
+            };
+        }
+        else if (event.pathParameters.fido2path === "register-authenticator/complete") {
+            logger.info("Completing the new authenticator registration ...");
+            const storedCredential = await handleCredentialsResponse(userHandle, parseBody(event));
+            return {
+                statusCode: 200,
+                body: JSON.stringify(storedCredential),
+                headers,
+            };
+        }
+        else if (event.pathParameters.fido2path === "authenticators/list") {
+            logger.info("Listing authenticators ...");
+            const rpId = event.queryStringParameters?.rpId;
+            if (!rpId) {
+                throw new UserFacingError("Missing RP ID");
+            }
+            if (!allowedRelyingPartyIds.includes(rpId)) {
+                throw new UserFacingError("Unrecognized RP ID");
+            }
+            const authenticators = await getExistingCredentialsForUser({
+                userId: userHandle,
+                rpId,
+            });
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    authenticators,
+                }),
+                headers,
+            };
+        }
+        else if (event.pathParameters.fido2path === "authenticators/delete") {
+            logger.info("Deleting authenticator ...");
+            const parsed = parseBody(event);
+            assertBodyIsObject(parsed);
+            logger.debug("CredentialId:", parsed.credentialId);
+            await deleteCredential({
+                userId: userHandle,
+                credentialId: parsed.credentialId,
+            });
+            return { statusCode: 204 };
+        }
+        else if (event.pathParameters.fido2path === "authenticators/update") {
+            const parsed = parseBody(event);
+            assertBodyIsObject(parsed);
+            await updateCredential({
+                userId: userHandle,
+                credentialId: parsed.credentialId,
+                friendlyName: parsed.friendlyName,
+            });
+            return { statusCode: 200, headers };
+        }
+        return {
+            statusCode: 404,
+            body: JSON.stringify({ message: "Not found" }),
+            headers,
+        };
+    }
+    catch (err) {
+        logger.error(err);
+        if (err instanceof UserFacingError)
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: err.message }),
+                headers,
+            };
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal Server Error" }),
+            headers,
+        };
+    }
+}
